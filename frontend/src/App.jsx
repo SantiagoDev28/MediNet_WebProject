4551f97f06188979f8559e57ec27e28ecef8f728
santiagodev28 <shurtado308@gmail.com>
2025-08-20 21:58:46 -0500

    Subiendo estructuracion de carpetas

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+     <div className= 'flex justify-center'>
+        <h1 className= 'text-5xl text-yellow-500 font-serif'>HELLO WORLD ;/ </h1>
+     </div>
+    </>
+  )
+}
+
+export default App
